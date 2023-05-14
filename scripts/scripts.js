d64c8072067d86a404a93ea21391e21cbca2d2f5
Lucas Molina <lucasivanmolina_bass@hotmail.com>
2023-05-14 02:53:24 -0300

    modo-oscuro

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,16 @@
-const boton = document.querySelector('.modo');
+const boton = document.querySelector(".modo");
 
 boton.addEventListener('click', () => {
-  document.body.classList.toggle('modo-oscuro');
+  document.body.classList.toggle("modo-oscuro");
+  document.querySelector(".modo").classList.toggle("modo-oscuro");
+  document.querySelector(".logo").classList.toggle("modo-oscuro");
+  document.querySelector("h1").classList.toggle("modo-oscuro");
+  document.querySelector(".user-text").classList.toggle("modo-oscuro");
+  document.querySelector("textarea").classList.toggle("modo-oscuro");
+  document.querySelector("p").classList.toggle("modo-oscuro");
+  document.querySelector(".btn-encr").classList.toggle("modo-oscuro");
+  document.querySelector(".btn-desencr").classList.toggle("modo-oscuro");
+  document.querySelector(".section-2").classList.toggle("modo-oscuro");
+  document.querySelector(".btn-copiar").classList.toggle("modo-oscuro");
+  document.querySelector(".mensaje").classList.toggle("modo-oscuro");
 });
